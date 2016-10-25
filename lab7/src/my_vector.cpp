c0a683085717b961b60914e17077bd8952a87616
myutman <myutman@gmail.com>
2016-10-26 00:49:59 +0300

    fixed

--- lab7/src/my_vector.cpp
+++ lab7/src/my_vector.cpp
@@ -40,7 +40,7 @@ void MyVector::reserve(std::size_t new_capacity){
 	if (new_capacity < _cp) return;
 	int *temp = new int[new_capacity];
 	_cp = new_capacity;
-	for (int i = 0; i < _sz; i++)
+	for (std::size_t i = 0; i < _sz; i++)
 		temp[i] = _data[i];
 	delete []_data;
 	_data = temp;
@@ -48,12 +48,12 @@ void MyVector::reserve(std::size_t new_capacity){
 
 void MyVector::resize(std::size_t new_size){
 	if (new_size > _cp){
-		int new_capacity = _cp;
+		std::size_t new_capacity = _cp;
 		while (new_capacity < new_size)
 			new_capacity += new_capacity;
 		reserve(new_capacity);
 	}
-	for (int i = _sz; i < new_size; i++)
+	for (std::size_t i = _sz; i < new_size; i++)
 		_data[i] = 0;
 	_sz = new_size;
 }
@@ -67,14 +67,14 @@ void MyVector::push_back(int value){
 void MyVector::insert(std::size_t index, int value){
 	resize(std::max(_sz + 1, index + 1));
 	if (index < _sz)
-		for (int i = _sz; i > index; i--)
+		for (std::size_t i = _sz; i > index; i--)
 			_data[i] = _data[i - 1];
 	_data[index] = value;
 }
 
 void MyVector::erase(std::size_t index){
 	assert(index < _sz);
-	for (int i = index; i < _sz - 1; i++)
+	for (std::size_t i = index; i < _sz - 1; i++)
 		_data[i] = _data[i + 1];
 	_sz--;
 }
