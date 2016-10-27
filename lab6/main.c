0488102602c7ba207102c6c9bd5938a510c7e5e8
myutman <myutman@gmail.com>
2016-10-27 08:57:42 +0300

    fixed

--- lab6/main.c
+++ lab6/main.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]){
 	book.capacity = 2;
 	book.size = 0;
 	book.humans = (human_t*) malloc(2 * sizeof(human_t));
-	printf("hello\n");
+	//printf("hello\n");
 	int err = load_phonebook_xml(argv[1], &book);
 	print_phonebook(&book);
 	clear_phonebook(&book);
