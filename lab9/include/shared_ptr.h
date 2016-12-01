24b6749e507ae3241071ab39a39fac9fda758c05
myutman <myutman@gmail.com>
2016-12-02 01:10:50 +0300

    done

---
+++ lab9/include/shared_ptr.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "matrix.h"
+#include <iostream>
+
+class shared_ptr {
+  public:
+    shared_ptr(Matrix* obj = 0);
+    shared_ptr(const shared_ptr& other);
+    shared_ptr& operator=(const shared_ptr &other);
+    ~shared_ptr();
+	void swap(shared_ptr& other);
+
+    Matrix* ptr() const;
+    bool isNull() const;
+    void reset(Matrix* obj = 0);
+
+    Matrix* operator->() const;
+    Matrix& operator*() const;
+
+  private:
+    class Storage {
+      public:	
+        Storage(Matrix* mtx){
+			//std::cerr << "hello\n";
+			data_ = mtx;
+			//std::cerr << "hello\n";
+		}
+        ~Storage(){
+			delete data_;
+		}
+
+        void incr(){
+			ref_count_++;
+		}
+        void decr(){
+			ref_count_--;
+			if (!ref_count_){
+				delete this;
+			}
+		}
+
+        int getCounter() const{
+			return ref_count_;
+		}
+        Matrix* getObject() const{
+			return data_;
+		}
+
+      private:
+        Matrix *data_;
+        int ref_count_;
+    };
+
+  private:
+    Storage *storage_;
+};
---
+++ lab9/src/shared_ptr.cpp
@@ -0,0 +1,47 @@
+#include "shared_ptr.h"
+#include <algorithm>
+
+shared_ptr::shared_ptr(Matrix* obj){
+	//std::cerr << "hello1\n";	
+	storage_ = new Storage(obj);
+	//std::cerr << "hello1\n";
+}
+
+shared_ptr::shared_ptr(const shared_ptr& other){
+	storage_ = other.storage_;
+	storage_->incr();
+}
+
+void shared_ptr::swap(shared_ptr& other){
+	std::swap(storage_, other.storage_);
+}
+
+shared_ptr& shared_ptr::operator=(const shared_ptr &other){	
+	shared_ptr tmp(other);
+	(*this).swap(tmp);
+	return *this;
+}
+
+shared_ptr::~shared_ptr(){
+	storage_->decr();
+}
+
+Matrix* shared_ptr::ptr() const{
+	return storage_->getObject();
+}
+
+bool shared_ptr::isNull() const{
+	return storage_->getObject() == NULL;
+}
+
+void shared_ptr::reset(Matrix* obj){	
+	storage_ = new Storage(obj);
+}
+
+Matrix* shared_ptr::operator->() const{
+	return ptr();
+}
+
+Matrix& shared_ptr::operator*() const{
+	return *ptr();
+}
